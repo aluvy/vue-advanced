6851f4b147b8e5d912edeed03cc55f7c0bdfa609
songmiok <aluvy_@naver.com>
2024-01-29 15:12:56 +0900

    Update HeaderNav.vue

--- vue-news/src/components/HeaderNav.vue
+++ vue-news/src/components/HeaderNav.vue
@@ -1,20 +1,21 @@
 <template>
   <header>
-    <h1><router-link to="/">logo</router-link></h1>
+    <h1><router-link to="/">Hacker News</router-link></h1>
     <nav>
       <ul>
-        <li><router-link to="/news">News</router-link></li>
-        <li><router-link to="/ask">Ask</router-link></li>
-        <li><router-link to="/jobs">Jobs</router-link></li>
+        <li><router-link to="/news">NEWS</router-link></li>
+        <li><router-link to="/jobs">JOBS</router-link></li>
+        <li><router-link to="/ask">ASK</router-link></li>
       </ul>
     </nav>
   </header>
 </template>
 
 <style scoped>
-header { display: flex; align-items: center; padding: 0.6rem 2rem; background: #42b883; }
-ul { display: flex; align-items: center; margin-left: 2rem; }
+header { display: flex; align-items: center; padding: 1rem 2rem; background: #42b883; }
+h1 { font-size: 1.6rem; color: #fff; font-weight: 700; }
+ul { display: flex; align-items: center; margin-left: 2.2rem; }
 li + li { margin-left: 2rem; }
-li a { font-size: 1.6rem; color: #fff; }
-li a.router-link-exact-active { color: #35495e }
+li a { font-size: 1.4rem; color: #fff; }
+li a.router-link-exact-active { color: #35495e; font-weight: 700 }
 </style>
