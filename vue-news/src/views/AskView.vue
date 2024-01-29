4f1b320d27da7ca2146bbc73364387a82290db24
songmiok <aluvy_@naver.com>
2024-01-29 15:14:10 +0900

    Update AskView.vue

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,38 +1,30 @@
 <template>
   <div>
-    <ul>
-      <li v-for="(item, i) in ask" :key="i">
-        <p>id: {{ item.id }}</p>
-        <p>comments_count: {{ item.comments_count }}</p>
-        <p>points: {{ item.points }}</p>
-        <p>time: {{ item.time }}</p>
-        <p>time_ago: {{ item.time_ago }}</p>
-        <p>title: {{ item.title }}</p>
-        <p>type: {{ item.type }}</p>
-        <p>url: {{ item.url }}</p>
-        <p>user: {{ item.user }}</p>
+    <ul class="item_list">
+      <li v-for="(item, i) in ask" :key="i" :id="item.id">
+        <strong>
+          <a :href="item.url">{{ item.title }}</a>
+        </strong>
+        <p>
+          <span>{{ item.points }} points by {{ item.user }}</span>
+          <span>{{ item.time_ago }}</span>
+          <span>{{ item.type }}</span>
+          <span>{{ item.comments_count }} comments</span>
+        </p>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-import { fetchAskList } from '../api/index';
+import { mapGetters } from 'vuex';
 
 export default {
-  data() {
-    return {
-      ask: []
-    }
+  computed: {
+    ...mapGetters(['ask']),
   },
   created() {
-    fetchAskList()
-      .then( res => this.ask = res.data )
-      .catch( e => console.log(e) )
+    this.$store.dispatch('FETCH_ASK');
   }
 }
 </script>
-
-<style>
-
-</style>
