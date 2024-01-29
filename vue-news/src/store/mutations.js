cbca931f937b03370da60d04711c3127972c0676
songmiok <aluvy_@naver.com>
2024-01-29 16:37:27 +0900

    Update mutations.js

--- vue-news/src/store/mutations.js
+++ vue-news/src/store/mutations.js
@@ -2,4 +2,5 @@ export default {
   SET_NEWS: (state, payload) => (state.news = payload),
   SET_JOBS: (state, payload) => (state.jobs = payload),
   SET_ASK: (state, payload) => (state.ask = payload),
+  SET_USER: (state, payload) => (state.user = payload),
 };
