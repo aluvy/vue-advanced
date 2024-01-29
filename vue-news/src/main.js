c072354c70fcdd692bf55b891ecd204722cd80a5
songmiok <aluvy_@naver.com>
2024-01-29 15:12:21 +0900

    Update main.js

--- vue-news/src/main.js
+++ vue-news/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
+import './assets/css/common.css';
 import { router } from './router/index.js';
-import './assets/css/reset.css';
+import { store } from './store/index.js';
 
 Vue.config.productionTip = true;
 
 new Vue({
   render: (h) => h(App),
   router,
+  store,
 }).$mount('#app');
