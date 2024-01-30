dd79b5744ddff6f401075250db0f05c404649819
songmiok <aluvy_@naver.com>
2024-01-30 16:46:27 +0900

    Update UserView.vue

--- vue-news/src/views/UserView.vue
+++ vue-news/src/views/UserView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="user_box">
-    <div class="photo"></div>
+    <div class="photo"><i class="fa-solid fa-user"></i></div>
     <ul class="info">
       <li>
         <span>user</span>
@@ -35,12 +35,14 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .user_box { display: flex; align-items: flex-start; padding: 2rem; }
-.user_box .photo { flex: 0 0 10rem; }
+.user_box .photo { position: relative; flex: 0 0 10rem; }
 .user_box .photo::before { content:''; display: block; width: 100%; padding-bottom: 100%; border-radius: 100%; background: #eee; box-shadow: inset 3px 3px 1rem rgba(0,0,0, 0.05) }
-.user_box .info { flex: 1 1 auto; margin-left: 2rem; }
+.user_box .photo i { position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%); font-size: 4rem; color: #fff; }
+.user_box .info { flex: 1 1 calc(100% - 10rem); margin-left: 2rem; }
 .user_box .info li { display: flex; align-items: flex-start; }
 .user_box .info li + li { margin-top: 0.4rem; }
 .user_box .info li span { flex: 0 0 10rem; }
+.user_box .info li p { flex: 1 1 calc(100% - 10rem); }
 </style>
