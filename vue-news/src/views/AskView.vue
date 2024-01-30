3d0a62c6e0602cf5bffad7e370e8ea8f142f79f1
songmiok <aluvy_@naver.com>
2024-01-30 16:45:53 +0900

    Update AskView.vue

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,30 +1,13 @@
 <template>
-  <div>
-    <ul class="item_list">
-      <li v-for="(item, i) in ask" :key="i" :id="item.id">
-        <strong>
-          <router-link :to="`/item/${item.id}`">{{ item.title }}</router-link>
-        </strong>
-        <p>
-          <span>{{ item.points }} points by {{ item.user }}</span>
-          <span>{{ item.time_ago }}</span>
-          <span>{{ item.type }}</span>
-          <span>{{ item.comments_count }} comments</span>
-        </p>
-      </li>
-    </ul>
-  </div>
+  <ListItem></ListItem>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import ListItem from '../components/ListItem.vue'
 
 export default {
-  computed: {
-    ...mapGetters(['ask']),
+  components: {
+    ListItem
   },
-  created() {
-    this.$store.dispatch('FETCH_ASK');
-  }
 }
 </script>
