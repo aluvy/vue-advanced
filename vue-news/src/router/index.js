4291ec585e7429af2213a9a05f70f60e95098b9a
songmiok <aluvy_@naver.com>
2024-01-31 17:00:26 +0900

    Update index.js

--- vue-news/src/router/index.js
+++ vue-news/src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import EventBus from '../utils/EventBus.js';
+import { store } from '../store/index.js';
+
 import NewsView from '../views/NewsView';
-import AskView from '../views/AskView';
-import JobsView from '../views/JobsView';
+// import AskView from '../views/AskView';
+// import JobsView from '../views/JobsView';
 import ItemView from '../views/ItemView';
 import UserView from '../views/UserView';
+
+import createListView from '../views/CreateListView.js'; // high order component
 
 Vue.use(VueRouter);
 
@@ -16,31 +21,52 @@ export const router = new VueRouter({
       redirect: '/news',
     },
     {
-      // path: url 주소에 대한 정보
       path: '/news',
-      // component: url 주소로 접속했을 때 표시될 컴포넌트
       name: 'news',
       component: NewsView,
+      // component: createListView('NewsView'),
+      beforeEnter: (to, from, next) => setBeforeEnter(to, from, next, 'FETCH_LIST'),
     },
     {
       path: '/jobs',
       name: 'jobs',
-      component: JobsView,
+      // component: JobsView,
+      component: createListView('JobsView'), // high order component
+      beforeEnter: (to, from, next) => setBeforeEnter(to, from, next, 'FETCH_LIST'),
     },
     {
       path: '/ask',
       name: 'ask',
-      component: AskView,
+      // component: AskView,
+      component: createListView('AskView'), // high order component
+      beforeEnter: (to, from, next) => setBeforeEnter(to, from, next, 'FETCH_LIST'),
     },
+
     {
       path: '/item/:id',
       name: 'item',
       component: ItemView,
+      beforeEnter: (to, from, next) => setBeforeEnter(to, from, next, 'FETCH_ITEM'),
     },
     {
       path: '/user/:id',
       name: 'user',
       component: UserView,
+      beforeEnter: (to, from, next) => setBeforeEnter(to, from, next, 'FETCH_USER'),
     },
   ],
 });
+
+const setBeforeEnter = (to, from, next, dispatchName) => {
+  let param = to.name;
+  if (dispatchName === 'FETCH_ITEM' || dispatchName === 'FETCH_USER') param = to.params.id;
+
+  EventBus.$emit('start:spinner');
+  store
+    .dispatch(dispatchName, param)
+    .then(() => {
+      // EventBus.$emit('end:spinner'); // mixin mounted()로 이동
+      next(); // next() 함수를 호출해야 페이지가 이동 된다.
+    })
+    .catch((e) => console.log(e));
+};
