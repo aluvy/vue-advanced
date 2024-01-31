f2e7007460c6e35df80aeef1d1e773f12b9f94ab
songmiok <aluvy_@naver.com>
2024-01-31 17:02:05 +0900

    Update App.vue

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -4,22 +4,47 @@
     <transition name="page">
       <router-view></router-view>
     </transition>
+    <loading-spinner :loading="loadingStatus"></loading-spinner>
   </div>
 </template>
 
 <script>
 import HeaderNav from './components/HeaderNav.vue'
+import LoadingSpinner from './components/LoadingSpinner.vue'
+import EventBus from './utils/EventBus.js'
 
 export default {
   components: {
-    HeaderNav
-  }  
+    HeaderNav,
+    LoadingSpinner
+  },
+  data() {
+    return {
+      loadingStatus: false,
+    }
+  },
+  methods: {
+    startSpinner() {
+      this.loadingStatus = true;
+    },
+    endSpinner() {
+      this.loadingStatus = false;
+    }
+  },
+  created() {
+    EventBus.$on('start:spinner', this.startSpinner);
+    EventBus.$on('end:spinner', this.endSpinner);
+  },
+  beforeDestroy() {
+    EventBus.$off('start:spinner', this.startSpinner);
+    EventBus.$off('end:spinner', this.endSpinner);
+  }
 }
 </script>
 
 <style scoped>
-.page-enter-active { position: absolute; animation: fade-in .3s .1s backwards; }
-.page-leave-active { position: absolute; animation: fade-in .1s reverse forwards; }
+.page-enter-active { position: absolute; width: 100%; animation: fade-in .3s .1s backwards; }
+.page-leave-active { position: absolute; width: 100%; animation: fade-in .1s reverse forwards; }
 
 @keyframes fade-in {
   0% { opacity: 0; }
