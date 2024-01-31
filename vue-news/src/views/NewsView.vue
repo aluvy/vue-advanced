afcb697ade1ccc76051c73b8b5a84c4729a6cd87
songmiok <aluvy_@naver.com>
2024-01-31 17:01:50 +0900

    Update NewsView.vue

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,13 +1,17 @@
 <template>
-  <ListItem></ListItem>
+  <div>
+    <list-item></list-item>
+  </div>
 </template>
 
 <script>
-import ListItem from '../components/ListItem.vue'
+import ListItem from '../components/ListItem.vue';
+import CommonMixin from '../mixins/CommonMixin'
 
 export default {
   components: {
     ListItem,
   },
+  mixins: [CommonMixin],
 }
 </script>
