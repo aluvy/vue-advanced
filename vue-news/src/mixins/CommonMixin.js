c7ee60df7a720f742d4e602b47d19915b90ae43d
songmiok <aluvy_@naver.com>
2024-01-31 17:00:04 +0900

    Create CommonMixin.js

---
+++ vue-news/src/mixins/CommonMixin.js
@@ -0,0 +1,19 @@
+import EventBus from '@/utils/EventBus';
+
+// mixin
+export default {
+  // 재사용 할 component option & logic | data, methdos, create 등...
+  // created() {
+  //   EventBus.$emit('start:spinner');
+  //   this.$store
+  //     .dispatch('FETCH_LIST', this.$route.name)
+  //     .then(() => {
+  //       // EventBus.$emit('end:spinner');
+  //     })
+  //     .catch((e) => console.log(e));
+  // },
+  mounted() {
+    console.log('common mixin');
+    EventBus.$emit('end:spinner');
+  },
+};
