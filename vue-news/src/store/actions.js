58e7cae4f482f06cc96b812389408d122d68d1e0
songmiok <aluvy_@naver.com>
2024-01-31 17:00:36 +0900

    Update actions.js

--- vue-news/src/store/actions.js
+++ vue-news/src/store/actions.js
@@ -1,30 +1,35 @@
-import { fetchNewsList, fetchJobsList, fetchAskList, fetchUserInfo, fetchItemView } from '../api/index';
+import { fetchList, fetchUserInfo, fetchItemView } from '../api/index';
 
 export default {
-  FETCH_NEWS(context) {
-    fetchNewsList()
-      .then(({ data }) => context.commit('SET_NEWS', data))
-      .catch((e) => console.log(e));
-  },
-  FETCH_JOBS(context) {
-    fetchJobsList()
-      .then((res) => context.commit('SET_JOBS', res.data))
-      .catch((e) => console.log(e));
-  },
-  FETCH_ASK(context) {
-    fetchAskList()
-      .then((res) => context.commit('SET_ASK', res.data))
+  // FETCH_NEWS(context) {
+  //   fetchNewsList()
+  //     .then((res) => context.commit('SET_NEWS', res.data))
+  //     .catch((e) => console.log(e));
+  // },
+  // FETCH_JOBS(context) {
+  //   fetchJobsList()
+  //     .then((res) => context.commit('SET_JOBS', res.data))
+  //     .catch((e) => console.log(e));
+  // },
+  // FETCH_ASK(context) {
+  //   fetchAskList()
+  //     .then((res) => context.commit('SET_ASK', res.data))
+  //     .catch((e) => console.log(e));
+  // },
+  FETCH_LIST(context, pageName) {
+    return fetchList(pageName)
+      .then(({ data }) => context.commit('SET_LIST', data))
       .catch((e) => console.log(e));
   },
   FETCH_USER(context, user) {
-    fetchUserInfo(user)
+    return fetchUserInfo(user)
       .then(({ data }) => {
         context.commit('SET_USER', data);
       })
       .catch((e) => console.log(e));
   },
   FETCH_ITEM(context, id) {
-    fetchItemView(id)
+    return fetchItemView(id)
       .then(({ data }) => {
         context.commit('SET_ITEM', data);
       })
