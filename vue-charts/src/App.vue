8b4a03228491bed11c36a22cd5fb39ee613a981e
songmiok <aluvy_@naver.com>
2024-02-01 16:24:59 +0900

    Create App.vue

---
+++ vue-charts/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Chart.js</h1>
+    <canvas id="myChart"></canvas>
+  </div>
+</template>
+
+<script>
+import { Chart, registerables } from 'chart.js'
+Chart.register(...registerables);
+
+export default {
+  mounted() {
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+}
+</script>
+
+<style>
+
+</style>
