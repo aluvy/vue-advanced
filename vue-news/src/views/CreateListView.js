6c31064c849cddc00d45def2f67e2c40e5c033b1
songmiok <aluvy_@naver.com>
2024-02-01 14:45:56 +0900

    Update CreateListView.js

--- vue-news/src/views/CreateListView.js
+++ vue-news/src/views/CreateListView.js
@@ -6,15 +6,6 @@ export default function createListView(name) {
   return {
     // 재사용 할 인스턴스(컴포넌트) 옵션들이 들어갈 자리
     name: name,
-    // created() {
-    //   EventBus.$emit('start:spinner');
-    //   this.$store
-    //     .dispatch('FETCH_LIST', this.$route.name)
-    //     .then(() => {
-    //       EventBus.$emit('end:spinner');
-    //     })
-    //     .catch((e) => console.log(e));
-    // },
     mounted() {
       console.log('high order component');
       EventBus.$emit('end:spinner');
