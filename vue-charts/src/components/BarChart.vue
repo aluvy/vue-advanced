9f796f731ef97c7a1853143af3bed971bdf82c51
songmiok <aluvy_@naver.com>
2024-02-02 15:45:38 +0900

    Create BarChart.vue

---
+++ vue-charts/src/components/BarChart.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <canvas ref="BarChart"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const ctx = this.$refs.BarChart;
+
+    new this.$_Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            this.$_ChartUtils.BGCOLOR.red,
+            this.$_ChartUtils.BGCOLOR.blue,
+            this.$_ChartUtils.BGCOLOR.yellow,
+            this.$_ChartUtils.BGCOLOR.green,
+            this.$_ChartUtils.BGCOLOR.purple,
+            this.$_ChartUtils.BGCOLOR.orange
+          ],
+          borderColor: [
+          this.$_ChartUtils.COLOR.red,
+            this.$_ChartUtils.COLOR.blue,
+            this.$_ChartUtils.COLOR.yellow,
+            this.$_ChartUtils.COLOR.green,
+            this.$_ChartUtils.COLOR.purple,
+            this.$_ChartUtils.COLOR.orange
+          ],
+          borderWidth: 1,
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+}
+</script>
