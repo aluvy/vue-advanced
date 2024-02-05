6ee2279f13f311659a7db8fdcca0f9336d0d6c51
songmiok <aluvy_@naver.com>
2024-02-05 10:27:31 +0900

    Create AppContent.vue

---
+++ design/common/src/components/AppContent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(a, i) in items" :key="i">
+        {{ a }}
+      </li>
+    </ul>
+    <button @click="$emit('renewItems')">renewItems</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    }
+  },
+}
+</script>
