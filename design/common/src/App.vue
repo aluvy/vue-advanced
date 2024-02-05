07073f38edae357c9faffaad50d02b4ae579af04
songmiok <aluvy_@naver.com>
2024-02-05 10:27:17 +0900

    Create App.vue

---
+++ design/common/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <AppHeader :appTitle="appTitle"></AppHeader>
+    <AppContent :items="items" @renewItems="renewItems"></AppContent>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppContent from './components/AppContent.vue'
+
+export default {
+  data() {
+    return {
+      appTitle: 'Common Approach',
+      items: [10, 20, 30],
+    }
+  },
+  methods: {
+    renewItems() {
+      this.items = [40, 50, 60];
+    }
+  },
+  components: {
+    AppHeader,
+    AppContent
+  }
+}
+</script>
+
+<style>
+
+</style>
