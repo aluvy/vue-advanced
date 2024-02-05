0f995b34acabdbaab20e8ef1e3e3816ad8f2ae92
songmiok <aluvy_@naver.com>
2024-02-05 15:44:05 +0900

    Update vue.config.js

--- vue-news/vue.config.js
+++ vue-news/vue.config.js
@@ -2,4 +2,6 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false, // eslint 끄기
+
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-advanced/vue-news/dist' : '/',
 });
