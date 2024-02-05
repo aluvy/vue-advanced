0cb1b46ca8448c1b77a8303de2de70e8f7b8fe6f
songmiok <aluvy_@naver.com>
2024-02-05 13:11:20 +0900

    Create App.vue

---
+++ design/controlled/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <CheckBox v-model="checked"></CheckBox>
+</template>
+
+<script>
+import CheckBox from './components/CheckBox.vue'
+
+export default {
+  components: {
+    CheckBox,
+  },
+  data() {
+    return {
+      checked: false,
+    }
+  },
+  methods: {
+    // toggle(event) {
+    //   console.log(event)
+    //   // this.$emit('input', !this.value);
+    // }
+  }
+  // created() {
+  //   fetchCheckBox()
+  //     .then(( result => this.checkboxes = result.data ))
+  //     .catch( e => console.log(e) )
+  // }
+}
+</script>
